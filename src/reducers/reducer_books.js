3a244f3455787632818664a19bfd21b6dc317d44
Esko Rikkonen <esko.rikkonen@gmail.com>
2018-01-26 22:52:01 +0200

    Lecture 42: mapStateToProps basics.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Go Problem 1'},
+        { title: 'Murky Things'},
+        { title: 'Daniel is not Home'},
+        { title: 'I\'m running out of ideas'},
+        { title: 'Oh, that\'s worked for you?!'}
+    ];
+}

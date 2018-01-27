595e1d7c539ababb7b5ff7aa7d82f94f95abd560
Esko Rikkonen <esko.rikkonen@gmail.com>
2018-01-27 23:58:56 +0200

    Lecture 46: Dispatcer beginnings

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/index';
 
 class BookList extends React.Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">
+                <li key={book.title} onClick={() => this.props.selectBook(book)} className="list-group-item">
                     {book.title}
                 </li>
             )
@@ -27,4 +29,8 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
